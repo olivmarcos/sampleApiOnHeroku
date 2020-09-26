3974ce71376ad29a69cc7a282736fc5b1f14211c
Marcos Oliveira <marcosoliveirasamcro@gmail.com>
2020-09-26 18:05:51 -0300

    disciplinas

--- controller.js
+++ controller.js
@@ -1,4 +1,5 @@
 const importData = require("./data.json");
+const importDiscipinas = require('./disciplinas.json')
 
 const getById = async (req, res) => {
     const [data] = await importData
@@ -6,7 +7,13 @@ const getById = async (req, res) => {
     res.send(item)
 }
 
+const getNotaByName = async (req, res) => {
+    const [data] = await importDiscipinas
+    const [item] = data.discipinas.filter(item => item.id === req.params.id)
+    res.send(item)
+}
 
 module.exports = {
-    getById
+    getById,
+    getNotaByName
 }
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const importData = require("./data.json");
-const { getById } = require("./controller")
+const importDiscipinas = require('./disciplinas.json')
+const { getById, getNotaByName } = require("./controller")
 let port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
@@ -12,8 +13,14 @@ app.get("/aulas", (req, res) => {
   res.send(importData);
 });
 
-
 app.get("/aula/:id", getById);
+
+app.get("/disciplinas", (req, res) => {
+  res.send(importDiscipinas);
+});
+
+app.get("/disciplina/:id", getNotaByName);
+
 
 app.listen(port, () => {
   console.log(`Example app is listening on port http://localhost:${port}`);
